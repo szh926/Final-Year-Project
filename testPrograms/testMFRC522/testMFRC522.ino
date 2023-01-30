f4a893055cfe59cc995cf09de31af2234abfda4d
szh926 <98886181+szh926@users.noreply.github.com>
2023-01-30 12:19:15 +0000

    added RFID reader test file

---
+++ testPrograms/testMFRC522/testMFRC522.ino
@@ -0,0 +1,19 @@
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <SPI.h>
+
+/*This code can be used to test an RFID RC522 module if you suspect any faults.
+ * Setup:
+ *  
+ * 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
